50645c4dba303d4fb88c7dddbc06c34de8253ffb
dibpuelma <edib@uc.cl>
2017-08-09 22:46:37 -0400

    First model schema

--- nora/models.py
+++ nora/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import uuid
 # Create your models here.
+
+class Option(models.Model):
+    description = models.CharField(max_length=100)
+
+class Menu(models.Model):
+    menu_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    options = models.ManyToManyField(Option)
+
+class UserChoice(models.Model):
+    user = models.OneToOneField(User)
+    menu = models.OneToOneField(Menu)
+    choice = models.OneToOneField(Option)
+    isBig = models.BooleanField()
+    additionalInfo = models.CharField(max_length=100)
